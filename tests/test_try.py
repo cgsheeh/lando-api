3f4d35b2c71099233570f7c8032b624e773769ae
Connor Sheehan <cosheehan@mozilla.com>
2023-05-31 18:35:55 -0400

    tests: add a test file for try stuff

---
+++ tests/test_try.py
@@ -0,0 +1,23 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import pytest
+
+
+# TODO implement
+def test_try_push_integration():
+    """End-to-end test of a try push landing job via the API."""
+    assert False, "Not implemented."
+
+
+# TODO implement
+def test_try_api():
+    """Test for the `/try` API."""
+    assert False, "Not implemented."
+
+
+# TODO implement
+def test_try_landing_job():
+    """Test that a Try landing job completes as expected."""
+    assert False, "Not implemented."
