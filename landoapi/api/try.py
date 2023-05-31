13e461ddbcaf34e0ccea5543b2c1c561354654d5
Connor Sheehan <cosheehan@mozilla.com>
2023-05-31 16:23:12 -0400

    enforce scm level 1 with a decorator

--- landoapi/api/try.py
+++ landoapi/api/try.py
@@ -2,6 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import functools
 import logging
 
 from connexion import ProblemException
@@ -18,29 +19,39 @@ from landoapi.storage import db
 logger = logging.getLogger(__name__)
 
 
+def enforce_scm_level_1(func):
+    """Decorator to enforce `active_scm_level_1` membership with error messaging."""
+
+    @functools.wraps(func)
+    def wrap_api(*args, **kwargs):
+        # Return appropriate error message if user does not have commit access.
+        if not g.auth0_user.is_in_groups("all_scm_level_1"):
+            raise ProblemException(
+                401,
+                "`scm_level_1` access is required.",
+                "You do not have `scm_level_1` commit access.",
+                type="https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/404",
+            )
+
+        # Check that user has active_scm_level_1 and not `expired_scm_level_1`.
+        if g.auth0_user.is_in_group(
+            "expired_scm_level_1"
+        ) or not g.auth0_user.is_in_groups("active_scm_level_1"):
+            raise ProblemException(
+                401,
+                "Your `scm_level_1` commit access has expired.",
+                "Your `scm_level_1` commit access has expired.",
+                type="https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/404",
+            )
+
+        return func(*args, **kwargs)
+
+    return wrap_api
+
+
 @auth.require_auth0(scopes=("lando", "profile", "email"), userinfo=True)
+@enforce_scm_level_1
 def post(data: dict):
-    # TODO move the scm check into a decorator.
-    # Return appropriate error message if user does not have commit access.
-    if not g.auth0_user.is_in_groups("all_scm_level_1"):
-        raise ProblemException(
-            401,
-            "`scm_level_1` access is required.",
-            "You do not have `scm_level_1` commit access.",
-            type="https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/404",
-        )
-
-    # Check that user has active_scm_level_1 and not `expired_scm_level_1`.
-    if g.auth0_user.is_in_group("expired_scm_level_1") or not g.auth0_user.is_in_groups(
-        "active_scm_level_1"
-    ):
-        raise ProblemException(
-            401,
-            "Your `scm_level_1` commit access has expired.",
-            "Your `scm_level_1` commit access has expired.",
-            type="https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/404",
-        )
-
     # TODO what format should the patch data be?
     patches = data["patches"]
 
