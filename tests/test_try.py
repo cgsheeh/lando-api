080245c04bd5e5f6a53e8d4471883737182b2eb1
Connor Sheehan <cosheehan@mozilla.com>
2023-06-01 23:06:32 -0400

    exception change

--- tests/test_try.py
+++ tests/test_try.py
@@ -8,19 +8,19 @@ import pytest
 # TODO implement
 def test_try_push_integration():
     """End-to-end test of a try push landing job via the API."""
-    assert False, "Not implemented."
+    raise NotImplemented("TODO")
 
 
 # TODO implement
 def test_try_api():
     """Test for the `/try` API."""
-    assert False, "Not implemented."
+    raise NotImplemented("TODO")
 
 
 # TODO implement
 def test_try_landing_job():
     """Test that a Try landing job completes as expected."""
-    assert False, "Not implemented."
+    raise NotImplemented("TODO")
 
 
 def test_scm_level_1_enforce():
